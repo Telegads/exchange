de08dd59991e8fa8f8e912a1e24acd33dfbd40e4
anerix <anerix@disroot.org>
2022-11-12 20:09:57 +0300

    #36 button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,32 @@
 import React, { FC } from "react";
 import style from "./Button.module.scss";
+import { Button as BootstrapButton } from 'react-bootstrap';
 
 type ButtonTypes = "primary" | "ghost";
-type Size = "S" | "M" | "L";
+type Size = 'lg' | 'sm';
 
 type ButtonProps = {
-  type: ButtonTypes;
-  rounded?: boolean;
-  size?: Size;
-  onClick: () => void;
-};
+  type: ButtonTypes,
+  size: Size,
+  onClick: () => void,
+  rounded?: boolean,
+  loading?: boolean,
+  disabled?: boolean,
+}
 
 export const Button: FC<ButtonProps> = ({
   type,
-  children,
+  size,
   rounded,
   onClick,
-  size = "M",
+  loading,
+  disabled,
+  children,
 }) => {
   return (
-    <button
-      className={`${style[type]} ${rounded ?? style.rounded} ${style[size]}`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <BootstrapButton variant={type} size={size} onClick={onClick} disabled={disabled}
+      className={`${style.btn} ${style[type]} ${style[size]} ${rounded ? style.rounded : ''} ${loading ? style.loading : ''}`}>
+        {loading ? 'Загрузка...' : children}
+    </BootstrapButton>
   );
 };
