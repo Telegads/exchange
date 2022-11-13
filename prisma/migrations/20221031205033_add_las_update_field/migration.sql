0eee1bf903ee262a5aa0724e9a7ebd96f73191b3
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-13 14:45:29 +0400

    feat: update scema and migrations

--- prisma/migrations/20221031205033_add_las_update_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `lastUpdateDateTime` to the `Channel` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Channel" ADD COLUMN     "lastUpdateDateTime" TIMESTAMP(3) NOT NULL;
