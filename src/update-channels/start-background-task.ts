1f618b77530a5da0802a4606eadf85263b33f99c
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-14 22:06:00 +0400

    feat: add connector deploy

--- src/update-channels/start-background-task.ts
+++ src/update-channels/start-background-task.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { getChannelsFromDbAndUpdate } from "./getChannelsFromDbAndUpdate";
 
-const task = cron.schedule("1 * * * *", () => {
+const task = cron.schedule("1 * * * * *", () => {
   getChannelsFromDbAndUpdate();
 });
 
