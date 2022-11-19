b3ae04837d7d73500152322f4d2048536e707c59
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-19 19:40:53 +0400

    cron time

--- src/update-channels/start-background-task.ts
+++ src/update-channels/start-background-task.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { getChannelsFromDbAndUpdate } from "./getChannelsFromDbAndUpdate";
 
-const task = cron.schedule("/10 * * * *", () => {
+const task = cron.schedule("*/10 * * * *", () => {
   getChannelsFromDbAndUpdate();
 });
 
