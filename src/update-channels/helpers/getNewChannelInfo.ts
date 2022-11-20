5f1ffc3bec4f2b21cb8bdaad4d50da2deda55f93
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-20 16:19:44 +0400

    add saving avatar path

--- src/update-channels/helpers/getNewChannelInfo.ts
+++ src/update-channels/helpers/getNewChannelInfo.ts
@@ -7,6 +7,7 @@ export type ChannelInfoResponse = {
   views_last_30_days: number;
   posts_last_30_days: number;
   views_last_day: number;
+  avatar_path: string;
 };
 
 export const getNewChannelInfo = (channelId: string) =>
