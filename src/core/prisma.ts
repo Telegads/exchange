0d71c3f16e75e69accea99a3ac268bf2aa065a19
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-26 16:36:36 +0400

    update prisma export

--- src/core/prisma.ts
+++ src/core/prisma.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  /* eslint-disable no-var */
+  // eslint-disable-next-line vars-on-top
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV !== 'production' ? ['info', 'warn', 'error'] : ['warn', 'error'],
+    errorFormat: 'pretty',
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 export default prisma;
