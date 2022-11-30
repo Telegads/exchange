a3b8ea72e32ff8bc8456d863c12bc8885354898c
Petr Nikitin <nikitin.p.94@gmail.com>
2022-11-30 22:18:12 +0400

    add favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,20 @@ import React from 'react';
 import { appWithTranslation } from 'next-i18next';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#783afa" />
+        <meta name="msapplication-TileColor" content="#783afa" />
+        <meta name="theme-color" content="#783afa" />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </SessionProvider>
