00f6bcddae888d4f2b003d252f3ebaa7f6e4ab9d
xNa1D <valentin@mindbox.cloud>
2022-12-10 21:11:51 +0300

    add locales to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,36 +30,34 @@ const MainPage: FC<MainPageProps> = () => {
           <div className={style.container}>
             <div className={style.advertising__title}>
               <h1>{t('index.title')}</h1>
-              <p className={style.text}>
-                Мы помогаем найти целевую аудиторию в Telegram и запускать эффективные рекламные кампании
-              </p>
+              <p className={style.text}>{t('index.subtitle')}</p>
               <a
                 href="https://docs.google.com/forms/d/1aV4RXuysmTOLIc-9rDN75zIrc94mkw4jV5br6HY-pxM/edit"
                 className={style.advertising__btn}
               >
-                Начать сейчас
+                {t('index.startbtn')}
               </a>
             </div>
             <div className={style.advantage}>
               <div className={`${style.advantage__user} ${style.advantage__item}`}>
                 <p className={style.advantage__namber}>207 228</p>
-                <p className={style.advantage__text}>новых пользователей на бирже</p>
+                <p className={style.advantage__text}>{t('advantage.newcustomers')}</p>
               </div>
               <div className={`${style.advantage__channel} ${style.advantage__item}`}>
                 <p className={style.advantage__namber}>7 754</p>
-                <p className={style.advantage__text}>каналов, проверенных вручную</p>
+                <p className={style.advantage__text}>{t('advantage.countchannels')}</p>
               </div>
               <div className={`${style.advantage__orders} ${style.advantage__item}`}>
                 <p className={style.advantage__namber}>460 482</p>
-                <p className={style.advantage__text}>успешно реализованных заказов</p>
+                <p className={style.advantage__text}>{t('advantage.countorders')}</p>
               </div>
             </div>
           </div>
         </section>
-        <section className={style.partners}>
+        {/* <section className={style.partners}>
           <div className={style.container}>
             <div className={style.partners__title}>
-              <h2>Нам доверяют</h2>
+              <h2>{t('partners.title')}</h2>
             </div>
             <div className={style.partners__items}>
               <div className={style.partners__google}>
@@ -76,7 +74,7 @@ const MainPage: FC<MainPageProps> = () => {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
         <section className={style.how_it_works}>
           <div className={style.container}>
             <div className={`${style.how_it_works__registration} ${style.how_it_works__items}`}>
@@ -86,13 +84,13 @@ const MainPage: FC<MainPageProps> = () => {
                   <img src="img/how_it_works/reg.svg" alt="" />
                 </div>
                 <div className={style.registration__subtitle}>
-                  <h3>Регистрируйте аккаунт</h3>
+                  <h3>{t('howitworks.stageone.title')}</h3>
                 </div>
               </div>
               <div className={style.registration__text}>
-                <p className={style.text}>Пройдите быструю регистрацию чтобы активировать аккаунт.</p>
+                <p className={style.text}>{t('howitworks.stageone.description')}</p>
                 <a href="https://docs.google.com/forms/d/1aV4RXuysmTOLIc-9rDN75zIrc94mkw4jV5br6HY-pxM/edit">
-                  регистрация
+                  {t('howitworks.stageone.textbtn')}
                 </a>
               </div>
             </div>
@@ -101,7 +99,7 @@ const MainPage: FC<MainPageProps> = () => {
             <div className={style.container}>
               <div className={`${style.how_it_works__catalog} ${style.how_it_works__items}`}>
                 <div className={style.catalog__text}>
-                  <p className={style.text}>Выберите подходящие каналы в каталоге</p>
+                  <p className={style.text}>{t('howitworks.stagetwo.description')}</p>
                 </div>
                 <div className={style.catalog__title}>
                   <div className={style.catalog__icon}>
@@ -109,7 +107,7 @@ const MainPage: FC<MainPageProps> = () => {
                     <img src="img/how_it_works/catalog.svg" alt="" />
                   </div>
                   <div className={style.catalog__subtitle}>
-                    <h3>Каталог каналов</h3>
+                    <h3>{t('howitworks.stagetwo.title')}</h3>
                   </div>
                 </div>
               </div>
@@ -123,12 +121,12 @@ const MainPage: FC<MainPageProps> = () => {
                   <img src="img/how_it_works/balance.svg" alt="" />
                 </div>
                 <div className={style.balance__subtitle}>
-                  <h3>Пополните баланс</h3>
+                  <h3>{t('howitworks.stagethree.title')}</h3>
                 </div>
               </div>
               <div className={style.balance__text}>
-                <p className={style.text}>Пополните баланс удобным способом:</p>
-                <p>Visa/MasterCard/МИР, Qiwi Банковский перевод</p>
+                <p className={style.text}>{t('howitworks.stagethree.description')}</p>
+                <p>{t('howitworks.stagethree.paylist')}</p>
                 <div className={style.balance__img}>
                   <img src="img/how_it_works/visa.svg" alt="" />
                   <img src="img/how_it_works/master.svg" alt="" />
@@ -141,34 +139,34 @@ const MainPage: FC<MainPageProps> = () => {
           <div className={style.container}>
             <div className={style.start_working__wrapper}>
               <div className={style.start_working__title}>
-                <h2>Начните работать с нами</h2>
+                <h2>{t('startworking.title')}</h2>
               </div>
               <div className={style.start_working__advantage}>
                 <div className={style.start_working__advertising}>
                   <div className={`${style.start_working__subtitle_advertising} ${style.start_working__subtitle}`}>
-                    <p>Рекламодателям</p>
+                    <p>{t('startworking.leftcircle.title')}</p>
                   </div>
                   <div className={style.line}></div>
                   <div className={style.start_working__text_advertising}>
-                    <p className={style.text}>Продвигайте свой бизнес с помощью эффективной рекламы в Telegram</p>
+                    <p className={style.text}>{t('startworking.leftcircle.description')}</p>
                   </div>
                 </div>
                 <div className={style.start_working__admin}>
                   <div className={`${style.start_working__subtitle_admin} ${style.start_working__subtitle}`}>
-                    <p>Администраторам</p>
+                    <p>{t('startworking.rightcircle.title')}</p>
                   </div>
                   <div className={style.line}></div>
                   <div className={style.start_working__text_admin}>
-                    <p className={style.text}>Зарабатывайте деньги на размещении рекламы в своих каналах и группах</p>
+                    <p className={style.text}>{t('startworking.rightcircle.description')}</p>
                   </div>
                 </div>
                 <div className={style.start_working__agency}>
                   <div className={`${style.start_working__subtitle_agency} ${style.start_working__subtitle}`}>
-                    <p>Агентствам</p>
+                    <p>{t('startworking.bottomcircle.title')}</p>
                   </div>
                   <div className={style.line}></div>
                   <div className={style.start_working__text_agency}>
-                    <p className={style.text}>Telegram – новый маркетинговый канал для ваших клиентов</p>
+                    <p className={style.text}>{t('startworking.bottomcircle.description')}</p>
                   </div>
                 </div>
               </div>
