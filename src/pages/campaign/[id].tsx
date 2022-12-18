2e83cc97f8cbbc8fb937210efd1d50e97f95dfca
xNa1D <valentin@mindbox.cloud>
2022-12-18 22:17:22 +0300

    fix common in locales

--- src/pages/campaign/[id].tsx
+++ src/pages/campaign/[id].tsx
@@ -43,7 +43,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
       props: {
-        ...(await serverSideTranslations(context.locale ?? 'en', ['campaign'])),
+        ...(await serverSideTranslations(context.locale ?? 'en', ['campaign', 'common'])),
         status: 'found',
         campaign,
       } as const,
