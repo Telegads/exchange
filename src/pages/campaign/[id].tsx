328f75e39ebdf98f48dc2c7f848f3a768260dc8a
Nikitin Petr <nikitin.p.94@gmail.com>
2023-01-01 12:49:33 +0300

    add style of edit page

--- src/pages/campaign/[id].tsx
+++ src/pages/campaign/[id].tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { Badge, ButtonGroup, Col, Container, Form, Image, Row } from 'react-bootstrap';
+import { Badge, Col, Container, Form, Row, Stack } from 'react-bootstrap';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
@@ -11,13 +11,14 @@ import { useRouter } from 'next/router';
 import { getParameterFromQuery } from '../../utils/getParameterFromQuery';
 import { options } from '../api/auth/[...nextauth]';
 import { captureException } from '../../core/sentry';
-import Layout from '../../components/Layout/Layout';
-import { Progress } from '../../components/Progress/Progress';
 import style from '../../scss/check.module.scss';
-import { Button } from '../../components/Button/Button';
 import { useUserNotification } from '../../hooks/useUserNotification';
 import { getCampaignById } from '../../features/campaigns/repository/getCampaignById';
 import { UpdateCampaignArgs } from '../../features/campaigns/repository/updateCampaign';
+import { Button } from '../../components/Button/Button';
+import Layout from '../../components/Layout/Layout';
+import { Progress } from '../../components/Progress/Progress';
+import { ScreenHeader } from '../../components/ScreenHeader/ScreenHeader';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   try {
@@ -96,8 +97,8 @@ const CampaignEditor = ({ campaign, status }: CampaignEditorProps) => {
         await axios.post<any, any, UpdateCampaignArgs>('/api/campaign/update', {
           id: campaign.id,
           status: 'DRAFT',
-          postText: data.postText,
           postImage: data.postImage,
+          postText: data.postText,
           channels: campaign.channels.map((channel) => ({ id: channel.id })),
         });
         notify('ok', 'success');
@@ -118,8 +119,8 @@ const CampaignEditor = ({ campaign, status }: CampaignEditorProps) => {
         await axios.post<any, any, UpdateCampaignArgs>('/api/campaign/update', {
           id: campaign.id,
           status: 'READY_FOR_MODERATION',
-          postText: data.postText,
           postImage: data.postImage,
+          postText: data.postText,
           channels: campaign.channels.map((channel) => ({ id: channel.id })),
         });
         notify('ok', 'success');
@@ -138,46 +139,92 @@ const CampaignEditor = ({ campaign, status }: CampaignEditorProps) => {
   return (
     <Layout>
       <Progress position={66} />
-      <Container>
-        <h1>
-          {t('edit.header')} <Badge bg="secondary">{campaign.status}</Badge>
-        </h1>
-
+      <Container fluid>
         <Form onSubmit={handleSubmit(saveDraft)}>
           <Row>
             <Col md={9}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>{t('edit.postImage')}</Form.Label>
-                <Form.Control type="text" {...register('postImage')} />
-                <Form.Text className="text-muted">Формат изображения png, jpg, GIF, не более 1280х1080px</Form.Text>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>{t('edit.postText')}</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  {...register('postText', { required: true })}
-                  isInvalid={Boolean(errors.postText)}
-                />
-                <Form.Text className="text-muted">
-                  максимум символов в посте с картинкой - 1000, без медиафайлов - 3500
-                </Form.Text>
-                <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
-              </Form.Group>
+              <div className={style.mainColumn}>
+                <ScreenHeader preHeader={t('edit.step') || undefined} subHeader={t('edit.subtitle') || undefined}>
+                  <Stack direction="horizontal" gap={4}>
+                    <Stack direction="horizontal" gap={2}>
+                      <h1 className={style.title}>{t('edit.header')}</h1>
+                      <button className={style.editButton}>
+                        <img className={style.editIcon} src="/img/icons/edit.svg" alt="edit" />
+                      </button>
+                    </Stack>
+                    <Badge className={style.draft}>{t(`status.${campaign.status}`)}</Badge>
+                  </Stack>
+                </ScreenHeader>
+                <div className={style.editFormSection}>
+                  <Form.Group className={style.editGroup} controlId="formBasicFile">
+                    <Form.Label className={style.editGroupTitle}>{t('edit.postImage')}</Form.Label>
+                    <Form.Control type="text" {...register('postImage', { required: true })} />
+                    <Form.Text className="text-muted">{t('edit.postImagePlaceholder')}</Form.Text>
+                    <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className={style.editGroup} controlId="exampleForm.ControlTextarea1">
+                    <Form.Label className={style.editGroupTitle}>{t('edit.postText')}</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={6}
+                      {...register('postText', { required: true })}
+                      isInvalid={Boolean(errors.postText)}
+                    />
+                    <Form.Text className="text-muted">{t('edit.postTextPlaceholder')}</Form.Text>
+                    <Form.Control.Feedback type="invalid">{t('edit.requiredField')}</Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </div>
             </Col>
-            <Col className={style.cartColumn}>
-              <div className={style.stickySidebar}>
-                <Image className={style.image} src={watch('postImage')} />
-                <p>{watch('postText')}</p>
-                <ButtonGroup vertical>
-                  <Button variant="primary" onClick={handleSubmit(saveForModeration)} disabled={!isValid}>
+            <Col md={3} className={style.cartColumn}>
+              <Stack gap={4}>
+                <Stack gap={3}>
+                  <h2 className={style.editSidebarTitle}>{t('edit.sidebar.title')}</h2>
+
+                  {watch('postImage') ? (
+                    <img
+                      className={style.editSidebarPreviewImgFull}
+                      width="100%"
+                      src={watch('postImage')}
+                      alt="preview"
+                    />
+                  ) : (
+                    <div className={style.emptyPreviewImage}>{t('edit.sidebar.emptyImage')}</div>
+                  )}
+
+                  {watch('postText') ? (
+                    <div>{watch('postText')}</div>
+                  ) : (
+                    <Stack gap={1}>
+                      <div className={style.emptyPreviewDescriptionLine}></div>
+                      <div className={style.emptyPreviewDescriptionLine}></div>
+                      <div className={style.emptyPreviewDescriptionLine}></div>
+                    </Stack>
+                  )}
+                </Stack>
+                <div className={style.editSidebarTotal}>
+                  <div className={style.editSidebarTotalName}>{t('edit.sidebar.total')}</div>
+                  <div className={style.editSidebarTotalValue}>Будет рассчитана позже</div>
+                </div>
+                <Stack gap={2}>
+                  <Button
+                    className={style.editSidebarSaveForModeration}
+                    variant="primary"
+                    onClick={handleSubmit(saveForModeration)}
+                    disabled={!isValid}
+                  >
                     {t('edit.saveForModeration')}
                   </Button>
-                  <Button variant="outline-primary" type="submit" disabled={!isValid}>
+                  <Button
+                    className={style.editSidebarSaveForDraft}
+                    variant="outline-primary"
+                    type="submit"
+                    disabled={!isValid}
+                  >
                     {t('edit.saveDraft')}
                   </Button>
-                </ButtonGroup>
-              </div>
+                </Stack>
+              </Stack>
             </Col>
           </Row>
         </Form>
